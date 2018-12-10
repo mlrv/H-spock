565413ea47341c2f9ca642140c8a3a862757294b
Marco Levrero <marco.levrero@gmail.com>
2018-12-10 20:55:43 +0000

    Main imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
-
-import Lib
-
-main :: IO ()
-main = someFunc
+  
+  import Web.Spock
+  import Web.Spock.Config
+  import Data.Aeson hiding (json)
+  import Data.Monoid ((<>))
+  import Data.Text (Text, pack)
+  import GHC.Generics
+  import Lib
+  
+  main :: IO ()
+  main = someFunc
