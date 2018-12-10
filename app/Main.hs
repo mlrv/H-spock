6298d6ece9ee71333ac362fdbfab799357b13517
Marco Levrero <marco.levrero@gmail.com>
2018-12-10 21:11:54 +0000

    Initial types and route

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-  
+
   import Web.Spock
   import Web.Spock.Config
   import Data.Aeson hiding (json)
@@ -10,6 +10,24 @@ module Main where
   import Data.Text (Text, pack)
   import GHC.Generics
   import Lib
+
+  data Person = Person
+    { name :: Text
+    , age  :: Int 
+    } deriving (Generic, Show)
+  
+  instance ToJSON Person
+  instance FromJSON Person
+
+  type Api = SpockM () () () ()
+  type ApiAction a = SpockAction () () () a
   
   main :: IO ()
-  main = someFunc
+  main = do
+    spockCfg <- defaultSpockCfg () PCNoDatabase ()
+    runSpock 8080 (spock spockCfg app)
+  
+  app :: Api
+  app = do
+    get "people" $ do
+      json $ [Person { name = "Marco", age = 25 }, Person { name = "Olivia", age = 27 }]
